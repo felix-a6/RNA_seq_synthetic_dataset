5f510428c07341cedfee763f94b5a0dfe7f4cb53
felix-a6 <trif1401@usherbrooke.ca>
2022-02-09 16:57:26 -0500

    initial commit

---
+++ generate_dataset.py
@@ -0,0 +1,11 @@
+import random
+import numpy as np
+import pandas as pd
+from math import *
+from pyfaidx import Fasta
+import itertools as itt
+import pickle
+import matplotlib.pyplot as plt
+import scipy.special as sps
+import os
+from multiprocessing import Pool
