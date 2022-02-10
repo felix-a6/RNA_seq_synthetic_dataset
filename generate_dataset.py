b0ff2c74179438cf6ed5be78612c43d8f956d4d3
felix-a6 <trif1401@usherbrooke.ca>
2022-02-10 13:17:02 -0500

    parametrize read simulation

--- generate_dataset.py
+++ generate_dataset.py
@@ -9,20 +9,139 @@ import matplotlib.pyplot as plt
 import scipy.special as sps
 import os
 from multiprocessing import Pool
+from os.path import join as opj
+import argparse
 
-# module number_reads.py
+ascii_chars = ['!',
+
+ '"',
+ '#',
+ '$',
+ '%',
+ '&',
+ "'",
+ '(',
+ ')',
+ '*',
+ '+',
+ ',',
+ '-',
+ '.',
+ '/',
+ '0',
+ '1',
+ '2',
+ '3',
+ '4',
+ '5',
+ '6',
+ '7',
+ '8',
+ '9',
+ ':',
+ ';',
+ '<',
+ '=',
+ '>',
+ '?',
+ '@',
+ 'A',
+ 'B',
+ 'C',
+ 'D',
+ 'E',
+ 'F',
+ 'G',
+ 'H',
+ 'I',
+ 'J',
+ 'K',
+ 'L',
+ 'M',
+ 'N',
+ 'O',
+ 'P',
+ 'Q',
+ 'R',
+ 'S',
+ 'T',
+ 'U',
+ 'V',
+ 'W',
+ 'X',
+ 'Y',
+ 'Z',
+ '[',
+ '\\',
+ ']',
+ '^',
+ '_',
+ '`',
+ 'a',
+ 'b',
+ 'c',
+ 'd',
+ 'e',
+ 'f',
+ 'g',
+ 'h',
+ 'i',
+ 'j',
+ 'k',
+ 'l',
+ 'm',
+ 'n',
+ 'o',
+ 'p',
+ 'q',
+ 'r',
+ 's',
+ 't',
+ 'u',
+ 'v',
+ 'w',
+ 'x',
+ 'y',
+ 'z',
+ '{',
+ '|',
+ '}',
+ '~']
+quality_levels = ascii_chars[:41][::-1]
 
 def nombre_reads_random(bins, numéro_bins):
     debut_bins = bins[numéro_bins]
     fin_bins = bins[numéro_bins+1] 
     return random.randint(debut_bins, fin_bins)  
 
+def generate_reads_distribution(output_dir, max_n_reads = 4000, n_bins = 251, n_transcript = 40000, high = (1000, 500), low = (0, 500)):
+    """combine 2 normal distributions high = (mean, standard dev) low = (mean, standard dev)
+    """
+    #Bins used for model
+    bins_made = np.linspace(0, max_n_reads, n_bins)
+    
+    #Poids des transcrits Distribution normale
+    s_high = np.random.normal(high[0], high[1], n_transcript) 
+    s_low = np.random.normal(low[0], low[1], n_transcript) 
+    count_high, bins_high, _ = plt.hist(s_high, bins = bins_made, density=False)
+    count_low, bins_low, _ = plt.hist(s_low, bins = bins_made, density=False)
 
-# module transcript_selection.py
+    #Sélection du plus grand compte des deux courbes
+    count_s = []
+    for n in range(n_bins - 1):
+        count_s.append(max([count_low[n], count_high[n]]))
+    count_s.append(0)
+    plt.plot(bins_made, count_s)
+    plt.savefig(opj(output_dir, 'model_read_distribution.png'))
+    plt.show()
 
-import number_reads
+    #Compte ajusté par pmf
+    count_sum = sum(count_s)
+    count_adjusted = [(x/count_sum)*n_transcript for x in count_s]
+    
+    return bins_made, count_adjusted
 
-def selection_transcrit(bins, count):
+def selection_transcrit(bins_made, count, selected_transcrits): # TODO Better function name
     selected_transcrits_copie = selected_transcrits.copy()
     numéro_bins = 0
     reads_par_transcrit = dict()
@@ -36,24 +155,31 @@ def selection_transcrit(bins, count):
         numéro_bins = numéro_bins+1
     return reads_par_transcrit
 
-# module quality_curve_shape.py
-
-def courbe_toutes_positions(scale):
-    shape = 2.
-    s = np.random.gamma(shape, scale, total_reads)
-    count_quality, bins_quality, _ = plt.hist(s, bins = quality_bins_made, density=False)
-    return count_quality
-
-# module quality_string.py
+def get_quality_pmfs(output_dir, total_reads, len_read = 75, quality_bins_made = np.linspace(0, 40, 40)):
+    positions = list(range(1, len_read+1))
+    scales = [1 + position * (4/len_read) for position in positions]
+    data = []
+    pmfs = []
+    
+    def courbe_toutes_positions(scale):
+        shape = 2.
+        s = np.random.gamma(shape, scale, total_reads)
+        count_quality, bins_quality, _ = plt.hist(s, bins = quality_bins_made, density=False)
+        return count_quality
+    
+    for scale in scales:
+        count = courbe_toutes_positions(scale)
+        data.append(count)
+        pmfs.append(count/sum(count))
+    pickle.dump(data, open(opj(output_dir, 'read_quality_distribution.pkl'), 'wb'))
+    return pmfs
 
 def get_quality_string(len_read):
     return ''.join([np.random.choice(quality_levels[:-2], p = pmfs[n]) for n in range(0, len_read)])
 
-# module get_read.py
-
-def make_reads(nom_transcrit, nombre_reads, len_read):
+def make_reads(nom_transcrit, nombre_reads, len_read, transcrits_sequences):
     reads=[]
-    transcrit = transcrits_ens[nom_transcrit]
+    transcrit = transcrits_sequences[nom_transcrit]
     for _ in range(nombre_reads):
         try:
             position_read = random.randint(0,len(transcrit)-len_read-1)
@@ -63,22 +189,49 @@ def make_reads(nom_transcrit, nombre_reads, len_read):
             raise e
     return reads
 
-# module get_all_reads.py
+def get_batches(n_batches, transcript_reads):
+    batch_size = int(np.ceil(1/n_batches * len(transcript_reads)))
+    transcript_reads_shuffle = list(transcript_reads.items())
+    random.shuffle(transcript_reads_shuffle)
+    batches = [transcript_reads_shuffle[n:n+batch_size] for n in range(0,  len(transcript_reads_shuffle), batch_size)]
+    return batches
 
-import get_read
-import quality_string
-
-def all_reads(len_read, nom_transcrit_nombre_reads, out_file):
+def write_batch_reads(len_read, nom_transcrit_nombre_reads, out_file, transcrits_sequences):
     with open(out_file,'w') as file_fastq:
         for n, (nom_transcrit, nombre_reads) in enumerate(nom_transcrit_nombre_reads):
-            reads = make_reads(nom_transcrit, nombre_reads, len_read)
+            reads = make_reads(nom_transcrit, nombre_reads, len_read, transcrits_sequences)
             for read in reads:
                 quality_string = get_quality_string(len_read)
                 file_fastq.write('\n'.join(['@' + read.fancy_name, str(read), '+', quality_string]) + '\n')
 
-# module helper.py
+def combine_files(output_dir):
+    with open(opj(output_dir, 'Reads_simulation.fastq'), 'w') as f_write:
+        for file in os.listdir(output_dir):
+            if 'Reads_simulation' in file:
+                with open(opj(output_dir, file), 'r') as f_read:
+                    for line in f_read:
+                        f_write.write(line)
+                # TODO delete batch file
 
-import get_all_reads
+def write_all_reads(output_dir, n_batches, transcrits_sequences, transcript_reads):
+    batches = get_batches(n_batches, transcript_reads)
 
-def helper_reads_writer(batch_num):
-    all_reads(75, batches[batch_num], f'/mnt/c/Users/xilef/Documents/Stage/Bioinfo/Reads_simulation_{batch_num}.fastq')
+    def helper_reads_writer(batch_num):
+        write_batch_reads(75, batches[batch_num], opj(output_dir, f'Reads_simulation_{batch_num}.fastq'), transcrits_sequences)
+
+    #Multiprocessing
+    with Pool(n_batches) as p:
+        print(p.map(helper_reads_writer, range(n_batches)))
+    combine_files(output_dir)
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Generate syntetic rna seq dataset')
+    parser.add_argument('fasta', type=str,
+                        help='fasta containing all transcript sequences')
+    parser.add_argument('output_dir', type=str,
+                        help='directory for all outputs')
+
+    args = parser.parse_args()
+    
+    transcrits_sequences = Fasta(args.fasta)
+    output_dir = args.output_dir
