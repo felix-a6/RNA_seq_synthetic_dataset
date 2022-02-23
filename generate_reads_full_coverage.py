cb51b4f1e1839e2c5df8413e66d583c358e68aab
felix-a6 <trif1401@usherbrooke.ca>
2022-02-23 11:13:54 -0500

    Initial commit

---
+++ generate_reads_full_coverage.py
@@ -0,0 +1,29 @@
+from pyfaidx import Fasta
+import argparse
+
+def truncate(seq, length=80):
+    return '\n'.join([seq[n:n+length] for n in range(0, len(seq), length)])
+
+def write_all_reads_fasta(transcript_fasta, len_read, out_file, step_size):
+    with open(out_file,'w') as all_reads_fasta:
+        for name, seq in transcript_fasta.items():
+            reads = [seq[position_read:(position_read+len_read)] for position_read in range(0, len(seq)-len_read, step_size)]
+            for read in reads:
+                all_reads_fasta.write('>' + name + '\n' + truncate(str(read),length=80) + '\n')
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Generate syntetic rna seq dataset')
+    parser.add_argument('fasta', type=str,
+                        help='fasta containing all transcript sequences')
+    parser.add_argument('out_file', type=str,
+                        help='fasta where output is')
+    parser.add_argument('len_read', type=int,
+                        help='length reads')
+    parser.add_argument('step_size', type=int, default = 1,
+                        help='nummber of nucleotide between reads')
+
+    args = parser.parse_args()
+
+    transcript_fasta = Fasta(args.fasta)
+
+    write_all_reads_fasta(transcript_fasta, args.len_read, args.out_file, args.step_size)
