591fc488ab78b12a4244c5c2a869728885606d3b
felix-a6 <trif1401@usherbrooke.ca>
2022-03-01 16:41:14 -0500

    change get batches

--- find_dark_region.py
+++ find_dark_region.py
@@ -3,6 +3,7 @@ from pyfaidx import Fasta
 from multiprocessing import Pool
 import argparse
 import pickle
+import numpy as np
 
 def normalise_coords(one_based, start, end):
     if one_based:
@@ -19,27 +20,26 @@ def stat_coverage_refimpl(samfile, chrom=None, start=None, end=None,
         reads = col.pileups
         yield {'chrom': chrom, 'pos': pos, 'reads_all': len(reads)}
 
-def find_dark_region(batches, n, mapping_file, gtf_file):
+def find_dark_region(batch, batch_num, mapping_file, output_dir):
     zero_reads = []
     no_reads = []
-    for gene in list(gtf_file.items())[n:batches[batches.index(n)+1]]:
-        region =  stat_coverage_refimpl(mapping_file, chrom=gene[1][0], start=gene[1][1], end=gene[1][2], one_based=False)
+    for gene_name, coords in batch:
+        chrom, start, end = coords
+        region =  stat_coverage_refimpl(mapping_file, chrom, start, end, one_based=False)
         region = [x['reads_all'] for x in region]
         if not region:
             no_reads.append(region)
             continue
         if min(region) == 0:
-            zero_reads.append(gene[0])
-    pickle.dump(zero_reads, open('zero_reads.pkl', 'wb'))
+            zero_reads.append(gene_name)
+    pickle.dump(zero_reads, open(f'{output_dir}/zero_reads{batch_num}.pkl', 'wb'))
 
-def multi_pross_function(batches):
-    for n in batches:
-        if batches.index(n) == len(batches)-1:
-            break
-    find_dark_region(batches, n, mapping_file, gtf_file)    
-
-def get_batches(n_batches, gtf_file):
-    batches = [n for n in range(0, len(gtf_file), len(gtf_file)/n_batches)]
+def get_batches(n_batches, gtf_file, mapping_file, output_dir):
+    batch_size = int(np.ceil(len(gtf_file)/n_batches))
+    batches = [
+        (list(gtf_file.items())[n:n+batch_size], n, mapping_file, output_dir)
+        for n in range(0, len(gtf_file), batch_size)
+        ]
     return batches
 
 if __name__=='__main__':
@@ -48,14 +48,16 @@ if __name__=='__main__':
                         help='file containing mapping output')
     parser.add_argument('gtf_file', type=str,
                         help='pickle transcripts infos')
+    parser.add_argument('output_dir', type=str,
+                        help='output direction')
 
     args = parser.parse_args()
     
     mapping_file = pysam.AlignmentFile(args.mapping_file, 'rb')
     gtf_file = pickle.load(open(args.gtf_file, 'rb'))
 
-    batches = get_batches(24, gtf_file)
+    batches = get_batches(24, gtf_file, mapping_file, args.output_dir)
         
     #Multiprocessing
     with Pool(24) as p:
-        p.starmap(multi_pross_function, batches)
+        p.starmap(find_dark_region, batches)
